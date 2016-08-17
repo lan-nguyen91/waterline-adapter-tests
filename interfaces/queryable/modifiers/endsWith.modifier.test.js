8ef1fde7ffe858bdbc58f8b83fbd9701939f66bb
Lan Nguyen <lan.nguyen@hart.com>
2016-08-17 11:46:17 -0700

    modify task and remove shorthand

--- interfaces/queryable/modifiers/endsWith.modifier.test.js
+++ interfaces/queryable/modifiers/endsWith.modifier.test.js
@@ -5,30 +5,6 @@ describe('Queryable Interface', function() {
 
   describe('Modifiers', function() {
     describe('endsWith', function() {
-      describe('shorthand', function() {
-
-        /////////////////////////////////////////////////////
-        // TEST METHODS
-        ////////////////////////////////////////////////////
-
-        it('should return the user with the correct name', function(done) {
-          var part = 'xxj8xa4hPFDH_short',
-              testName = 'endsWith query test xxj8xa4hPFDH_short';
-
-          Queryable.User.create({ first_name: testName }, function(err) {
-            if(err) return done(err);
-
-            Queryable.User.endsWith({ first_name: part }, function(err, users) {
-              assert.ifError(err);
-              assert(Array.isArray(users));
-              assert.equal(users.length, 1);
-              assert.equal(users[0].first_name, testName);
-              done();
-            });
-          });
-        });
-      });
-
       describe('full where criteria', function() {
 
         /////////////////////////////////////////////////////
@@ -47,30 +23,6 @@ describe('Queryable Interface', function() {
               assert(Array.isArray(users));
               assert.equal(users.length, 1);
               assert.equal(users[0].first_name, testName);
-              done();
-            });
-          });
-        });
-      });
-
-      describe('dynamic attribute', function() {
-
-        /////////////////////////////////////////////////////
-        // TEST METHODS
-        ////////////////////////////////////////////////////
-
-        it('should have [attribute]EndsWith() method', function(done) {
-          var part = 'xxj8xrxh!!!r',
-              testType = 'Dynamic EndsWith test' + part;
-
-          Queryable.User.create({ type: testType }, function(err) {
-            if(err) return done(err);
-
-            Queryable.User.typeEndsWith(part, function(err, users) {
-              assert.ifError(err);
-              assert(Array.isArray(users));
-              assert.equal(users.length, 1);
-              assert.equal(users[0].type, testType);
               done();
             });
           });
